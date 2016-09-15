dfe6bd705ff16c4d1d626f4ebce5cfeb6ea6c667
Jacob Dein <jake@jacobdein.com>
2016-09-15 12:04:10 -0400

    Adds ability for 'analysis.psd' to return the mean psd and fixes minor errors

--- wavescape/analysis.py
+++ wavescape/analysis.py
@@ -13,7 +13,15 @@ from scipy.fftpack import fft
 from scipy.signal import spectrogram, get_window
 
 
-def psd(wave, units = 'decibels', scaling = 'density', 
+def _psd_mean():
+	# compute psd mean (RMS mean)
+	psd_mean = ( psd.sum(axis = 2) / psd.shape[2] )
+	
+	# convert to decibels
+	psd_mean = 10 * np.log10(psd_mean / (pressure_reference**2))
+
+
+def psd(wave, units = 'decibels', scaling = 'density', kind = 'spectrogram',
 		window_length = 1000, window_overlap = 50, window_shape = 'hann', 
 		pressure_reference = 20.):
 	"""
@@ -29,6 +37,9 @@ def psd(wave, units = 'decibels', scaling = 'density',
 		
 	scaling: string, default = 'density'
 		result scaling, 'spectrum' or 'density'
+	
+	kind: string, default = 'spectrogram'
+		result type, 'spectrogram', 'mean', or 'both'
 	
 	window_length: integer, default = 1000
 		length of analysis window in number of samples
@@ -47,6 +58,8 @@ def psd(wave, units = 'decibels', scaling = 'density',
 	# check parameters
 	if units not in ['decibels', 'watts']:
 		raise ValueError("'{0}' are not acceptable units".format(units))
+	if kind not in ['spectrogram', 'mean', 'both']:
+		raise ValueError("'{0}' is not an acceptable kind".format(kind))
 	
 	# convert window_overlap percent value to decimal value
 	window_overlap = window_overlap / 100.
@@ -64,12 +77,27 @@ def psd(wave, units = 'decibels', scaling = 'density',
 										 noverlap = window_length * window_overlap, 
 										 return_onesided = True, 
 										 scaling = scaling)
+	
+	# compute psd mean (RMS mean)
+	if kind in ['mean', 'both']:
+		psd_mean = ( psd.sum(axis = 2) / psd.shape[2] )
 		
 	# convert to decibels
 	if units == 'decibels':
-		return f, t, 10 * np.log10(psd / (pressure_reference**2))
+		if kind == 'mean':
+			f, t, 10 * np.log10(psd_mean / (pressure_reference**2))
+		elif kind == 'both':
+			return f, t, 10 * np.log10(psd / (pressure_reference**2)), 10 * np.log10(psd_mean / (pressure_reference**2))
+		else:
+			return f, t, 10 * np.log10(psd / (pressure_reference**2))
+	# return watts
 	else:
-		return f, t, psd
+		if kind == 'mean':
+			return f, t, psd_mean
+		elif kind == 'both':
+			return f, t, psd, psd_mean
+		else:
+			return f, t, psd
 
 
 def sel(wave, units = 'decibels', bin_width = 1000, 
@@ -109,13 +137,10 @@ def sel(wave, units = 'decibels', bin_width = 1000,
 	# check parameters
 	if units not in ['decibels', 'watts']:
 		raise ValueError("'{0}' are not acceptable units".format(units))
-	
-	# convert window_overlap percent value to decimal value
-	window_overlap = window_overlap / 100.
 		
 	# compute power spectrum
-	f, t, pss = psd(wave, units = 'watts', scaling = 'spectrum', 
-					window_length = window_length, window_overlap = 50, window_shape = 'hann')
+	f, t, pss = psd(wave, units = 'watts', scaling = 'spectrum',
+					window_length = window_length, window_overlap = window_overlap, window_shape = window_shape)
 	
 	# compute and apply 'b' term
 	w = get_window(window_shape, window_length, fftbins = True)
